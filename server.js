553b44624e6e38c2c6508fef5ebed8cd559938ba
David Youster <davidyouster@gmail.com>
2016-03-24 03:04:30 +0000

    Implement basic POST handling.

--- server.js
+++ server.js
@@ -5,10 +5,30 @@ const router = require('./router');
 const HOST = '127.0.0.1';
 const PORT = 8081;
 
+const data = {
+  post: '',
+  pathname: '',
+  response: null
+}
+
+const onData = function onData(chunk) {
+  console.log('Received POST data chunk (' + chunk + ')...');
+  data.post += chunk;
+}
+
+const onEnd = function onEnd() {
+  router.route(data.pathname, data.response)
+  data.post = '';
+  data.pathname = '';
+  data.response = null;
+}
+
 const onRequest = function onRequest(request, response) {
-  var pathname = url.parse(request.url).pathname;
-  console.log('Request received for ' + pathname);
-  router.route(pathname, response);
+  data.pathname = url.parse(request.url).pathname;
+  data.response = response;
+  console.log('Request received for ' + data.pathname);
+  request.addListener('data', onData);
+  request.addListener('end', onEnd);
 }
 
 const startServing = function startServing() {
