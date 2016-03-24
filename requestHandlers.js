e2b78061aeea0fe910f4dceee4e6490b1810c326
David Youster <davidyouster@gmail.com>
2016-03-24 02:42:16 +0000

    Setup Swig templating.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,11 @@
+const swig = require('swig');
+
 const index = function index(response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('The index page');
+  const template = swig.compileFile('./templates/index.html');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(template());
   response.end();
+
 }
 
 const upload = function upload(response) {
--- router.js
+++ router.js
@@ -5,7 +5,7 @@ handle['/'] = requestHandlers.index;
 handle['/index'] = requestHandlers.index;
 handle['/home'] = requestHandlers.index;
 handle['/upload'] = requestHandlers.upload;
-handle[404] = requestHandlers.status404
+handle[404] = requestHandlers.status404;
 
 const route = function route(pathname, response) {
   console.log('Routing request for ' + pathname);
