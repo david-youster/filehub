349a525b53ca86d0743c467834f41531fc3bd67d
David Youster <davidyouster@gmail.com>
2016-03-26 18:50:00 +0000

    Display network IP on startup.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const os = require('os');
 const router = require('./router');
 
 const HOST = '0.0.0.0';
@@ -13,7 +14,11 @@ function onRequest(request, response) {
 
 function startServing() {
   http.createServer(onRequest).listen(PORT, HOST);
-  console.log('Server listening on ' + HOST + ':' + PORT);
+  console.log('Server listening on ' + getNetworkIP() + ':' + PORT);
+}
+
+function getNetworkIP() {
+  return os.networkInterfaces().wlan0[0].address;
 }
 
 module.exports.startServing = startServing;
