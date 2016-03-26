f43b58be9880fed4662aa8a5d478c52e5e8a6d19
David Youster <davidyouster@gmail.com>
2016-03-26 18:09:03 +0000

    Render list of uploaded files on index page.

--- db.js
+++ db.js
@@ -4,11 +4,30 @@ const connectionURL = "mongodb://localhost:27017/localex";
 
 const client = mongodb.MongoClient;
 
-function addUpload(data) {
+function saveUploadData(uploadData) {
   client.connect(connectionURL, function (error, db) {
     db.collection('uploads').insert(data);
     db.close();
   }); 
 }
 
-module.exports.addUpload = addUpload;
+function getUploads(response, renderFunction) {
+  client.connect(connectionURL, function (error, db) {
+    db.collection('uploads').find().toArray(function (error, uploads) {
+      uploads = uploads.map(extractUploadData);
+      renderFunction(response, {uploads: uploads});
+      db.close()
+    });
+  });
+}
+
+function extractUploadData(doc) {
+  return {
+    id: doc._id.toString(),
+    name: doc.name,
+    path: doc.path
+  };
+}
+
+module.exports.saveUploadData = saveUploadData;
+module.exports.getUploads = getUploads;
--- requestHandlers.js
+++ requestHandlers.js
@@ -4,31 +4,34 @@ const util = require('util');
 const db = require('./db');
 
 function index(response) {
+  db.getUploads(response, renderIndexPage);
+}
+
+function renderIndexPage(response, templateLocals) {
   const template = swig.compileFile('./templates/index.html');
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write(template());
+  response.write(template(templateLocals));
   response.end();
-
 }
 
 function upload(response, request) {
   const form = new formidable.IncomingForm();
   form.uploadDir = './uploads';
-  form.parse(request, readUpload);
+  form.parse(request, readUploadData);
   response.writeHead(302, {'Location': '/'});
   response.end();
 }
 
-function readUpload(error, fields, files) {
+function readUploadData(error, fields, files) {
   if (error) {
     console.error(error);
     return;
   }
-  const data = extractUploadData(files.file);
-  db.addUpload(data);
+  const data = extractFileData(files.file);
+  db.saveUploadData(data);
 }
 
-function extractUploadData(file) {
+function extractFileData(file) {
   return {
     name: file.name,
     path: file.path
